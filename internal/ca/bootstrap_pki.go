a6d86891a7f59a501a275d5b5338e6f6b1184a9a
Leon <leonv11@gmx.net>
2026-06-04 21:52:07 +0000

    feat(ca): implement bootstrap PKI functions for CA creation
    * Added functions to bootstrap the creation of Root and Intermediate
      Certificate Authorities (CAs) using the `smallstep/certificates`
      library. This includes `bootstrapCACreator`, `generateBootstrapRoot`,
      and `generateBootstrapIntermediate`, which facilitate the
      initialization of a PKI structure with specified configurations.
    * The new functions enhance the CA engine's capabilities by allowing
      for automated CA generation, improving the overall setup process
      for users. Robust error handling is included to ensure reliable
      operation during CA creation and certificate writing.

---
+++ internal/ca/bootstrap_pki.go
@@ -0,0 +1,143 @@
+package ca
+
+import (
+	"context"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.step.sm/crypto/pemutil"
+
+	"github.com/smallstep/certificates/cas"
+	"github.com/smallstep/certificates/cas/apiv1"
+	"github.com/smallstep/certificates/pki"
+
+	"github.com/ARCOOON/arx-ca/internal/config"
+)
+
+func bootstrapCACreator() (apiv1.CertificateAuthorityCreator, error) {
+	svc, err := cas.New(context.Background(), apiv1.Options{
+		Type:      apiv1.SoftCAS,
+		IsCreator: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("create softcas: %w", err)
+	}
+	creator, ok := svc.(apiv1.CertificateAuthorityCreator)
+	if !ok {
+		return nil, errors.New("softcas does not implement CertificateAuthorityCreator")
+	}
+	return creator, nil
+}
+
+func generateBootstrapRoot(
+	p *pki.PKI,
+	creator apiv1.CertificateAuthorityCreator,
+	boot config.CABootstrapConfig,
+	resource string,
+	pass []byte,
+) (*apiv1.CreateCertificateAuthorityResponse, error) {
+	keyReq := boot.KeyCreateRequest()
+	subject := bootstrapSubject(boot.RootCN, boot.Organization, boot.Country)
+
+	resp, err := creator.CreateCertificateAuthority(&apiv1.CreateCertificateAuthorityRequest{
+		Name:     resource + "-Root-CA",
+		Type:     apiv1.RootCA,
+		Lifetime: 10 * 365 * 24 * time.Hour,
+		CreateKey: &apiv1.CreateKeyRequest{
+			Name:               p.RootKey[0],
+			SignatureAlgorithm: keyReq.SignatureAlgorithm,
+			Bits:               keyReq.Bits,
+		},
+		Template: &x509.Certificate{
+			Subject:               subject,
+			KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+			BasicConstraintsValid: true,
+			IsCA:                  true,
+			MaxPathLen:            1,
+			MaxPathLenZero:        false,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("create root certificate authority: %w", err)
+	}
+
+	if resp.KeyName != "" {
+		p.RootKey[0] = resp.KeyName
+	}
+	if err := p.WriteRootCertificate(resp.Certificate, resp.PrivateKey, pass); err != nil {
+		return nil, fmt.Errorf("write root certificate: %w", err)
+	}
+	return resp, nil
+}
+
+func generateBootstrapIntermediate(
+	p *pki.PKI,
+	creator apiv1.CertificateAuthorityCreator,
+	boot config.CABootstrapConfig,
+	resource string,
+	parent *apiv1.CreateCertificateAuthorityResponse,
+	pass []byte,
+) error {
+	keyReq := boot.KeyCreateRequest()
+	subject := bootstrapSubject(boot.IntermediateCN, boot.Organization, boot.Country)
+
+	resp, err := creator.CreateCertificateAuthority(&apiv1.CreateCertificateAuthorityRequest{
+		Name:     resource + "-Intermediate-CA",
+		Type:     apiv1.IntermediateCA,
+		Lifetime: 10 * 365 * 24 * time.Hour,
+		CreateKey: &apiv1.CreateKeyRequest{
+			Name:               p.IntermediateKey,
+			SignatureAlgorithm: keyReq.SignatureAlgorithm,
+			Bits:               keyReq.Bits,
+		},
+		Template: &x509.Certificate{
+			Subject:               subject,
+			KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+			BasicConstraintsValid: true,
+			IsCA:                  true,
+			MaxPathLen:            0,
+			MaxPathLenZero:        true,
+		},
+		Parent: parent,
+	})
+	if err != nil {
+		return fmt.Errorf("create intermediate certificate authority: %w", err)
+	}
+
+	p.Files[p.Intermediate] = encodeCertificatePEM(resp.Certificate)
+
+	if resp.KeyName != "" {
+		p.IntermediateKey = resp.KeyName
+	}
+	if resp.PrivateKey != nil {
+		keyPEM, err := encodeEncryptedPrivateKey(resp.PrivateKey, pass)
+		if err != nil {
+			return fmt.Errorf("encode intermediate private key: %w", err)
+		}
+		p.Files[p.IntermediateKey] = keyPEM
+	}
+	return nil
+}
+
+func bootstrapSubject(commonName, organization, country string) pkix.Name {
+	subject := pkix.Name{
+		CommonName:   commonName,
+		Organization: []string{organization},
+	}
+	if country != "" {
+		subject.Country = []string{country}
+	}
+	return subject
+}
+
+func encodeEncryptedPrivateKey(key interface{}, pass []byte) ([]byte, error) {
+	block, err := pemutil.Serialize(key, pemutil.WithPassword(pass))
+	if err != nil {
+		return nil, err
+	}
+	return pem.EncodeToMemory(block), nil
+}
